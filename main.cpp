392db4dc5f3f63f33dd893df1eaa9114395260b2
Vadim <prozorov09vadim@yandex.ru>
2019-03-29 18:27:48 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <climits>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout<<argc<<endl;
+	for (int i=0; i<argc; i++) {
+		cout<< argv[i] << endl;
+	}
+	system ("pause");
+	return 0;
+}
